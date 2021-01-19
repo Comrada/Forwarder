b7f94725d91b48b6760462ecf7b85a9ba3242c39
Pavel Lobyrin <pavel@lobyrin.ru>
2021-01-19 16:03:05 +0100

    add a Spring application

---
+++ src/main/java/com/github/comrada/forwarder/ForwarderApplication.java
@@ -0,0 +1,12 @@
+package com.github.comrada.forwarder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForwarderApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(ForwarderApplication.class, args);
+    }
+}
